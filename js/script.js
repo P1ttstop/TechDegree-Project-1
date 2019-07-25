303fa1366e1a07ff0eb6111077322d6e609320fe
Darryl Pitt <darrylpitt43@gmail.com>
2019-07-25 16:36:53 -0400

    updates to script.js
    Added new functions for background color changes and automatic quote changes

--- js/script.js
+++ js/script.js
@@ -12,26 +12,30 @@ let quotes = [
     quote: "Learning is a journey. View missteps as an opportunity to learn and move forward.",
     source: "Emily Schweiss",
     citation: "Blog",
-    year: 2017
-
+    year: 2017,
+    tag: "#coding-positivity"
   },
   {
     quote: "When something is important enough, you do it even if the odds are not in your favor.",
-    source: "Elon Musk"
+    source: "Elon Musk",
+    tag: "#technology"
     },
   {
     quote: "The seed must grow regardless of the fact that it’s planted in stone.",
-    source: "Tupac Shakur"
+    source: "Tupac Shakur",
+    tag: "#modern-day-philosopher"
   },
   {
     quote: "By giving people the power to share, we're making the world more transparent.",
     source: "Mark Zuckerberg",
-    citation: "Facebook"
+    citation: "Facebook",
+    tag: "#genius"
   },
   {
     quote: "Great achievers are driven, not so much by the pursuit of success but by the fear of failure.",
     source: "Larry Ellison",
-    citation: "Oracle"
+    citation: "Oracle",
+    tag: "#motivated"
   },
   {
     quote: "You don’t have to thank me. I absorbed you. We’re practically related.",
@@ -48,23 +52,39 @@ function getRandomQuote() {
   return randomNumber;
 }
 
-/***
-  Create the `printQuote` function to: 
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND 
-     the random quote variable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/
+//this function sets a random value to the red, blue, and green colors
+function randomColor() {
+  //sets x, y, and z to random number from 0 to 256
+    let x = Math.floor(Math.random() * 256);
+    let y = Math.floor(Math.random() * 256);
+    let z = Math.floor(Math.random() * 256);
+
+    //this variable holds the string of the rgb value
+    let color = "rgb(" + x + "," + y + "," + z + ")";
+
+    //sets the body of the html file to the a random color
+    document.body.style.backgroundColor = color;
+    //the buttons color changes to the same random color as the body
+    document.getElementById('loadQuote').style.background = color;
+}
+
+//after 20 seconds a new quote will be printed to the screen
+function changeQuote() {
+  let change = window.setInterval(printQuote, 20000);
+  return change;
+}
+
+//this function is responsible for printing the quote to the page
 function printQuote() {
   //calls the getRandomQuote function
   let randQuote = getRandomQuote();
+  let addElement;
+  let text;
+  let next;
+  let span = document.createElement('SPAN');
   let htmlString = "";
-  htmlString += '<p class="quotes">' + randQuote.quote + '</p>';
+  //Adds the quote and source to the html page
+  htmlString += '<p class="quote">' + randQuote.quote + '</p>';
   htmlString += '<p class="source">' + randQuote.source;
   //If the quote has a key: citation a span element will appear in the string
   if(randQuote.citation) {
@@ -74,16 +94,18 @@ function printQuote() {
   if(randQuote.year) {
     htmlString += '<span class="year">' + randQuote.year + '</span>';
   }
+  if(randQuote.tag) {
+    htmlString += span.innerHTML = ", " + randQuote.tag;
+    document.getElementById("quote-box").appendChild(span);
+  }
   htmlString += '</p>';
   //sets the div element with id "quote-box" to the string above
   document.getElementById("quote-box").innerHTML = htmlString;
+  //calls the randomColor function
+  randomColor();
+  //calls the changQuote function
+  changeQuote();
 }
 
-/***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
-  comment.
-***/
-
+//allows the button tag to change the quote when clicked
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
